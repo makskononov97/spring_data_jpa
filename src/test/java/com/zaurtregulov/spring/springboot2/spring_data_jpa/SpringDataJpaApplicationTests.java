16312ebd876b52840e2b378716532c4b85dfe732
makskononov97 <115967472+makskononov97@users.noreply.github.com>
2022-10-24 17:10:23 +0700

    Initial commit

---
+++ src/test/java/com/zaurtregulov/spring/springboot2/spring_data_jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.springboot2.spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
